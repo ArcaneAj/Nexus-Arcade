e278b560d380c5f21f6724e8601c7f527b95c163
ArcaneAj <41121914+ArcaneAj@users.noreply.github.com>
2024-08-16 02:27:57 +1200

    Added computed properties

--- src/app/price-result/price-result.component.ts
+++ src/app/price-result/price-result.component.ts
@@ -1,5 +1,6 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { PriceResult } from '../models/price-result.model';
+import { CraftResult } from '../models/craft-result.model';
 
 @Component({
     selector: 'app-price-result',
@@ -9,5 +10,32 @@ import { PriceResult } from '../models/price-result.model';
     styleUrl: './price-result.component.scss'
 })
 export class PriceResultComponent {
-    result = input.required<PriceResult>();
+    public result = input.required<PriceResult>();
+    public name = computed(() => this.result().name);
+    public marketPrice = computed(() => this.result().marketPrice);
+    public shopPrice = computed(() => this.result().shopPrice);
+    public craftedPrice = computed(() => this.getCheapestCraftResult(this.result().craftedPrices));
+    public shopProfit = computed(() => this.getProfit(this.result().shopPrice, this.result().marketPrice));
+    public craftProfit = computed(() => this.getProfit(this.craftedPrice()?.price, this.result().marketPrice));
+
+    getProfit(buyPrice?: number, sellPrice?: number): number {
+        if (sellPrice == null || buyPrice == null) {
+            return 0;
+        }
+
+        if (sellPrice! <= buyPrice!) {
+            return 0;
+        }
+
+        return sellPrice! - buyPrice!;
+    }
+
+    
+    getCheapestCraftResult(craftResults: CraftResult[] | null): CraftResult | undefined {
+        if (craftResults === null || craftResults.length === 0) {
+            return undefined;
+        }
+
+        return craftResults.sort((a,b) => (a.price > b.price) ? 1 : ((b.price > a.price) ? -1 : 0))[0]
+    }
 }
