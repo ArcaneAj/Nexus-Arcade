0a9dd9cf0ccecc3db3f44fee13ffcfeac99e356a
ArcaneAj <41121914+ArcaneAj@users.noreply.github.com>
2024-08-24 23:20:05 +1200

    Removed logging

--- src/app/price-result/price-result-dialog/price-result-dialog.component.ts
+++ src/app/price-result/price-result-dialog/price-result-dialog.component.ts
@@ -34,7 +34,6 @@ export class PriceResultDialogComponent {
         result: PriceResult,
         },
     ) {
-        console.log(data.result.history);
         this.configureGraph();
     }
     configureGraph() {
