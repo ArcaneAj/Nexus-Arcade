f77782f0d2d36bfb59eeae591831171457a0923d
ArcaneAj <41121914+ArcaneAj@users.noreply.github.com>
2024-08-30 14:23:34 +1200

    Added button click animations for feedback

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,3 +1,10 @@
+import {
+    animate,
+    state,
+    style,
+    transition,
+    trigger,
+} from '@angular/animations';
 import { CommonModule } from '@angular/common';
 import { Component, input, output } from '@angular/core';
 
@@ -7,15 +14,41 @@ import { Component, input, output } from '@angular/core';
     imports: [CommonModule],
     templateUrl: './button.component.html',
     styleUrl: './button.component.scss',
+    animations: [
+        trigger('onClick', [
+            state(
+                'clean',
+                style({
+                    opacity: 1,
+                })
+            ),
+            state(
+                'dirty',
+                style({
+                    opacity: 0.8,
+                })
+            ),
+            transition('clean => dirty', [animate('0.2s')]),
+            transition('dirty => clean', [animate('0.2s')]),
+        ]),
+    ],
 })
 export class ButtonComponent {
     public clicked = output<void>();
     public height = input<string>();
     public width = input<string>();
     public disabled = input<boolean>();
+
+    public isDirty = false;
     mouseDown() {
-        if (this.disabled() !== true) {
-            this.clicked.emit();
+        if (this.disabled() === true) {
+            return;
         }
+
+        this.isDirty = true;
+        this.clicked.emit();
+        setTimeout(() => {
+            this.isDirty = false;
+        }, 200);
     }
 }
